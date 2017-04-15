efd77787664731a6b09410dad9be46751747181a
Matthijs Kamstra <matthijs@fonk.amsterdam>
2017-04-15 22:42:53 +0200

    initial commit

---
+++ src/Html.hx
@@ -0,0 +1,175 @@
+package;
+
+
+// https://www.w3.org/TR/Html401/struct/global.Html#h-7.5.4
+// div span
+/**
+ *  id, class (document-wide identifiers)
+lang (language information), dir (text direction)
+title (element title)
+style (inline style information)
+align (alignment)
+onclick, ondblclick, onmousedown, onmouseup, onmouseover, onmousemove, onmouseout, onkeypress, onkeydown, onkeyup (intrinsic events)
+ */
+typedef DivAttribute = {
+	@:optional var id : String;
+	@:optional var clas : String;
+	@:optional var lang : String;
+	@:optional var dir : String;
+	@:optional var title : String;
+	@:optional var style : String;
+	@:optional var align : String;
+}
+// https://developer.mozilla.org/en-US/docs/Web/HTML/Attributes
+
+// https://developer.mozilla.org/en-US/docs/Web/HTML/Global_attributes
+typedef Attribute = {
+	// custom att
+	@:optional var name : String;
+	@:optional var text : String;
+
+	// default
+	@:optional var accesskey:String;
+	@:optional var clas:String; // class is a reserved word
+	@:optional var contenteditable:String;
+	@:optional var contextmenu:String;
+	@:optional var data:String;
+	@:optional var dir:String;
+	@:optional var draggable:String;
+	@:optional var dropzone:String;
+	@:optional var hidden:String;
+	@:optional var id:String;
+	@:optional var itemid:String;
+	@:optional var lang:String;
+	@:optional var spellcheck:String;
+	@:optional var style:String;
+	@:optional var tabindex:String;
+	@:optional var title:String;
+};
+
+// https://developer.mozilla.org/en-US/docs/Web/HTML/Element/a
+typedef AAttribute = {
+	> Attribute,
+	@:optional var download:String;
+	@:optional var href:String;
+	@:optional var hreflang:String;
+	@:optional var referrerpolicy:String;
+	@:optional var rel:String;
+	@:optional var target:Target;
+	@:optional var type:String;
+}
+
+@:enum abstract Target (String) {
+	var BLANK = '_blank';
+	var SELF = '_self';
+	var PARENT = '_parent';
+	var TOP = '_top';
+}
+
+class Html {
+
+	// start
+	public static function html(?elements:Array<El>):El{ return new El('', {}, elements); }
+
+	// meteor
+	public static function template(?att:Attribute, ?elements:Array<El>) : El { return new El('template', att, elements); }
+
+	// I am not going to write down ALL elements
+	// https://developer.mozilla.org/en-US/docs/Web/HTML/Element
+	public static function el(el:String, ?att:Attribute, ?elements:Array<El>) : El { return new El(el, att, elements); }
+
+	public static function div(?att:Attribute, ?elements:Array<El>) : El { return new El('div', att, elements); }
+	public static function p(?att:Attribute, ?elements:Array<El>) : El { return new El('p', att, elements); }
+	public static function button(?att:Attribute, ?elements:Array<El>) : El { return new El('button', att, elements); }
+
+	public static function a(?att:AAttribute, ?elements:Array<El>) : El { return new El('a', att, elements); }
+
+	public static function comment(?att:Attribute, ?elements:Array<El>) : El { return new El('comment', att, elements); }
+
+	public static function h1(?att:Attribute, ?elements:Array<El>) : El { return new El('h1', att, elements); }
+	public static function h2(?att:Attribute, ?elements:Array<El>) : El { return new El('h2', att, elements); }
+	public static function h3(?att:Attribute, ?elements:Array<El>) : El { return new El('h3', att, elements); }
+	public static function h4(?att:Attribute, ?elements:Array<El>) : El { return new El('h4', att, elements); }
+	public static function h5(?att:Attribute, ?elements:Array<El>) : El { return new El('h5', att, elements); }
+	public static function h6(?att:Attribute, ?elements:Array<El>) : El { return new El('h6', att, elements); }
+
+}
+
+class El {
+
+	private var name : String;
+	private var att:Attribute;
+	private var elements:Array<El> = [];
+
+	private static var _html = '';
+
+	public function new (name:String, att:Attribute, elements:Array<El>){
+		this.name = name;
+		this.att = att;
+		this.elements = elements;
+	}
+
+
+	private function convert(name:String, att:Attribute, elements:Array<El>, count:Int = 0):String
+	{
+		//trace("Html.convert: ", name, att, elements, tab);
+
+		var tab = '';
+		for ( i in 0 ... count ) tab += '\t';
+
+		var isDone = false;
+
+		if(name != ''){
+
+			if(name == 'comment'){
+				_html += '$tab<!--';
+			} else {
+				_html += '$tab<$name';
+			}
+
+			var attArr = Reflect.fields(att);
+			for ( j in 0 ... attArr.length ) {
+				var _att = attArr[j];
+				var _attName = attArr[j];
+				if(_att == 'clas') _attName = 'class'; // reserved word
+				if(_att == 'text') continue;
+				_html += ' $_attName="'+Reflect.field(att,_att)+'"';
+			}
+
+			if(att.text != null){
+				if(name == "comment"){
+					_html += ' ${att.text} -->\n';
+				} else{
+					_html += '>${att.text}</$name>\n';
+				}
+				isDone = true;
+			} else {
+				_html += '>\n';
+			}
+
+			if(elements != null){
+				count++;
+				for ( i in 0 ... elements.length ) {
+					var el = elements[i];
+					convert(el.name, el.att, el.elements, count);
+				}
+			}
+			if(!isDone) _html += '$tab</$name>\n';
+		} else {
+			_html += '<!-- This template is generated with Haxe. Do not edit! -->\n';
+			// html root?
+			for ( i in 0 ... elements.length ) {
+				var el = elements[i];
+				convert(el.name, el.att, el.elements, 0);
+			}
+		}
+		return _html;
+	}
+
+	public function toString(){
+		return convert(name, att, elements);
+	}
+
+}
+
+
---
+++ src/Main.hx
@@ -0,0 +1,55 @@
+package;
+
+import Html;
+import Html.*;
+
+/**
+ * @author Matthijs Kamstra aka [mck]
+ */
+class Main {
+
+	public function new () {
+		// var html = html([
+		// 	template({name:"pageone"})
+		// ]);
+
+
+		// var html = html([
+		// 	template({name:"pageone"},[
+		// 		div({clas:'container'},[
+		// 			h1({text:'PAGEONE'})
+		// 		])
+		// 	])
+		// ]);
+
+		var templateName = 'pageone';
+		var html = html([
+			template({name:templateName},[
+				comment({text:'Start template: ${templateName}'}),
+				div({clas:'container'},[
+					h1({text:'${templateName.toUpperCase()}'}),
+					p({text: 'template for ${templateName}'}),
+					p({text: "{{test}}"}),
+					p({title:"p-title",text:"hello"}),
+					el("address",{id:"address-id",text:"not one I code for"}),
+					a({href:"http://www.haxe.org",text: "haxe.org",target:Target.BLANK,title:"title"}),
+					button({clas:"testBtn", text:"click"})
+				])
+			])
+		]);
+
+		#if neko
+			Sys.println('---------');
+			Sys.println(html.toString());
+			sys.io.File.saveContent('bin/_data/test.html', html.toString());
+			Sys.println('---------');
+		#else
+			trace(html.toString());
+		#end
+
+	}
+
+	static public function main () {
+		var app = new Main ();
+	}
+}
